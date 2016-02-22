432626c15b7aa5854ca03f975f1069314048be8f
Jeremy D <jeremy.dormitzer@gmail.com>
2016-02-22 01:17:17 -0700

    finished about page

--- public/javascripts/home.js
+++ public/javascripts/home.js
@@ -18,7 +18,9 @@ $(document).ready(function() {
 	$(".about").click(function(event) {
 		event.preventDefault();
 		$('#main-content').empty();
-		$('#main-content').html(load_about());
+		load_about(function(data) {
+			$('#main-content').html(data);
+		});
 	});	
 });
 
@@ -35,7 +37,7 @@ function load_blog(callback) {
 							"<div class='col s10 push-s1'>" +
 							//	"<div class='card green lighten-3'>" +
 							//		"<div>" +
-								"<h1>" + this.title.$t +"</h1>" +
+								"<h2>" + this.title.$t +"</h2>" +
 								"<p class='flow-text'>" + content + "</p>" +
 								"<div class='divider'></div>" +
 							//		"</div>" +
@@ -51,8 +53,8 @@ function load_projects() {
 	return "These are my projects!";
 }
 
-function load_about() {
+function load_about(callback) {
 	$.get('/html/about.html', function(data) {
-		return data;
+		callback(data);
 	});
 }
